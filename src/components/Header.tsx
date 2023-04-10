5bc81b598bd26c183b0065ce9e682e522ba9e421
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2023-04-11 00:13:31 +1000

    Added header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { css } from '@emotion/css';
+import { ReactNode } from 'react';
+
+type HeaderProps = {
+  children: ReactNode;
+  title: string;
+};
+
+export const Header = ({ children, title }: HeaderProps) => {
+  return (
+    <div className={getHeaderStyle()}>
+      { title && (<h1 className={getTitleStyle()}>{title}</h1>) }
+      {children}
+    </div>
+  );
+};
+
+const getHeaderStyle = () => {
+  return css({
+    display: 'flex',
+    padding: '1rem',
+    gap: '1rem',
+    alignItems: 'center',
+  });
+};
+
+const getTitleStyle = () => {
+  return css({
+    fontSize: '1rem',
+    flexShrink: 0,
+  });
+}
